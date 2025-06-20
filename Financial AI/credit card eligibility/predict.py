d88612d0ba8c0bd655c487006e981d87db0e1360
giriraj-roy-7723 <roy.2004002@gmail.com>
2025-06-20 22:01:45 +0530

    model+prediction

--- Financial AI/credit card eligibility/predict.py
+++ Financial AI/credit card eligibility/predict.py
@@ -17,37 +17,56 @@ def predict_eligibility(user_input: dict):
             df[col] = 0
     df = df[model.feature_names_in_]  # reorder correctly
 
-    # ✅ Ensure it's a DataFrame with valid column names
-    print("\n🧪 Final columns passed to model:", df.columns.tolist())
-
     # Predict
     prediction = model.predict(df)[0]
     return "✅ Eligible" if prediction == 1 else "❌ Not Eligible"
 
-
-# Sample
-if __name__ == "__main__":
-    user_input = {
+test_profiles = [
+    {
         "Gender": 1,
         "Own_car": 1,
         "Own_property": 0,
+        "Work_phone": 1,
+        "Phone": 1,
+        "Email": 1,
+        "Unemployed": 0,
+        "Num_children": 0,
+        "Num_family": 2,
+        "Account_length": 36,
+        "Total_income": 500000,
+        "Age": 30,
+        "Years_employed": 6,
+        "Income_type": "Working",
+        "Education_type": "Higher education",
+        "Family_status": "Married",
+        "Housing_type": "House / apartment",
+        "Occupation_type": "Accountants"
+    },
+    {
+        "Gender": 0,
+        "Own_car": 0,
+        "Own_property": 1,
         "Work_phone": 0,
-        "Phone": 0,
+        "Phone": 1,
         "Email": 0,
         "Unemployed": 0,
         "Num_children": 0,
         "Num_family": 2,
-        "Account_length": 35,
-        "Total_income": 112500.0,
-        "Age": 32.296351,
-        "Years_employed": 5.842694,
+        "Account_length": 31,
+        "Total_income": 157500.0,
+        "Age": 27.463945187101718,
+        "Years_employed": 4.021985393266117,
         "Income_type": "Working",
-        "Education_type": "Higher education",
+        "Education_type": "Secondary / secondary special",
         "Family_status": "Married",
         "Housing_type": "House / apartment",
         "Occupation_type": "Laborers"
     }
 
-    result = predict_eligibility(user_input)
-    print("Prediction:", result)
+   
 
+]
+if __name__ == "__main__":
+    for i, profile in enumerate(test_profiles, 1):
+        result = predict_eligibility(profile)
+        print(f"Prediction {i}: {result}")
