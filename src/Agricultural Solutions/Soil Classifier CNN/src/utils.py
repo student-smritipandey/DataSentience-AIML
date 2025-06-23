e593556ff8ca0a3d57bf3a64552d93e3b9061f6a
giriraj-roy-7723 <roy.2004002@gmail.com>
2025-06-24 01:21:06 +0530

    model built

---
+++ src/Agricultural Solutions/Soil Classifier CNN/src/utils.py
@@ -0,0 +1,28 @@
+# src/utils.py
+
+import matplotlib.pyplot as plt
+
+def plot_training_history(history):
+    plt.figure(figsize=(12, 4))
+
+    # Accuracy
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['accuracy'], label='Train Accuracy')
+    plt.plot(history.history['val_accuracy'], label='Val Accuracy')
+    plt.title('Accuracy over Epochs')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    # Loss
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['loss'], label='Train Loss')
+    plt.plot(history.history['val_loss'], label='Val Loss')
+    plt.title('Loss over Epochs')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.savefig("../images/training_plot.png")
+    plt.show()
